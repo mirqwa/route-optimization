b069b6adb48978746d36843fa3f97bd940da5cba
mirqwa <kimrodrikwa@gmail.com>
2025-02-16 11:10:04 +0300

    get distances

---
+++ q_learning.py
@@ -0,0 +1,22 @@
+import argparse
+
+import utils
+
+
+def main(api_key: str) -> None:
+    g_maps_client = utils.get_gmaps_client(api_key)
+    cities_locations_gdf = utils.get_cities_coordinates(
+        g_maps_client, use_saved_coordinates=True
+    )
+    utils.plot_cities(cities_locations_gdf)
+    distances = utils.get_intercity_distances(
+        cities_locations_gdf, g_maps_client, use_saved_distances=True
+    )
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--api_key", type=str, required=True)
+
+    args = parser.parse_args()
+    main(args.api_key)
--- utils.py
+++ utils.py
@@ -1,5 +1,4 @@
 import argparse
-import random
 from pathlib import Path
 
 import contextily as cx
