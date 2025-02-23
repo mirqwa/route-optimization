8d0fc0d67fa5d98d094c25ff02afcf0cbb95fc38
mirqwa <kimrodrikwa@gmail.com>
2025-02-23 19:03:30 +0300

    initialize q table

--- sarsa.py
+++ sarsa.py
@@ -1,8 +1,37 @@
 import argparse
 
+import geopandas as gpd
 import numpy as np
 
 import utils
+
+
+NO_OF_NEIGHBORS = 10
+
+
+def train_agent(
+    cities_locations_gdf: gpd.GeoDataFrame,
+    num_episodes: int,
+    start_city_index: str,
+    end_city_index: str,
+    distances: np.ndarray,
+) -> np.ndarray:
+    q_table = utils.initialize_q_table(distances, NO_OF_NEIGHBORS)
+
+
+def get_optimal_path(
+    cities_locations_gdf: gpd.GeoDataFrame,
+    distances: np.ndarray,
+    start_city: str,
+    end_city: str,
+) -> list:
+    start_city_index = cities_locations_gdf[
+        cities_locations_gdf["Label"] == start_city
+    ].index[0]
+    end_city_index = cities_locations_gdf[
+        cities_locations_gdf["Label"] == end_city
+    ].index[0]
+    train_agent(cities_locations_gdf, 1000, start_city_index, end_city_index, distances)
 
 
 def main(api_key: str):
@@ -10,12 +39,15 @@ def main(api_key: str):
     cities_locations_gdf = utils.get_cities_coordinates(
         g_maps_client, use_saved_coordinates=True
     )
-    utils.plot_cities(cities_locations_gdf)
+    # utils.plot_cities(cities_locations_gdf)
     distances = utils.get_intercity_distances(
         cities_locations_gdf, g_maps_client, use_saved_distances=True
     )
     distances = distances / 1000
     distances = np.where(distances == 0, float("inf"), distances)
+    shortest_path, route = get_optimal_path(
+        cities_locations_gdf, distances, "Nairobi", "Kampala"
+    )
 
 
 if __name__ == "__main__":
