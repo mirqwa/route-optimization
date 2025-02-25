7a9a712683a948fd2d731662153d8e8a3513cdb5
mirqwa <kimrodrikwa@gmail.com>
2025-02-25 12:25:46 +0300

    initialize state action values

--- off_policy_monte_carlo.py
+++ off_policy_monte_carlo.py
@@ -1,6 +1,25 @@
 import argparse
 
+import geopandas as gpd
+import numpy as np
+
 import utils
+
+
+EPISODES = 10000
+NO_OF_NEIGHBORS = 10
+
+
+def get_optimal_path(
+    cities_locations_gdf: gpd.GeoDataFrame, distances: np.ndarray
+) -> tuple:
+    policy = utils.initialize_policy(distances, NO_OF_NEIGHBORS)
+    state_action_values = utils.initialize_state_action_values(
+        cities_locations_gdf, policy
+    )
+
+    for episode in range(EPISODES):
+        pass
 
 
 def main(api_key: str) -> None:
