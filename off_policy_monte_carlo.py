9ca62f355982e330139d46244c6da5281fe08f05
mirqwa <kimrodrikwa@gmail.com>
2025-02-25 13:26:40 +0300

    update target policy

--- off_policy_monte_carlo.py
+++ off_policy_monte_carlo.py
@@ -6,7 +6,11 @@ import numpy as np
 import utils
 
 
-EPISODES = 10000
+np.random.seed(0)
+
+EPISODES = 1000
+EPSILON = 0.2
+DISCOUNT_FACTOR = 0.95
 NO_OF_NEIGHBORS = 10
 
 
@@ -34,8 +38,35 @@ def get_optimal_path(
         episode_results = utils.generate_episode(
             behavior_policy, start_city_index, end_city_index
         )
+        G = 0
+        W = 1
+        for state, action in reversed(episode_results):
+            G = DISCOUNT_FACTOR * G - distances[state][action]
+            C[state][action] += W
+            state_action_values[state][action] = (
+                1 - W / C[state][action]
+            ) * state_action_values[state][action] + (W / C[state][action]) * G
+            action_with_max_value = np.argmax(state_action_values[state])
+            target_policy = utils.update_policy(
+                target_policy, EPSILON, state, action_with_max_value
+            )
+            if action != action_with_max_value:
+                break
+            try:
+                W = W / (behavior_policy[state][action])
+            except Exception:
+                breakpoint()
 
-    shortest_path = route = None
+    shortest_path, route = utils.get_shortest_path(
+        state_action_values, start_city_index, end_city_index
+    )
+
+    route_distance = utils.get_distance(distances, route)
+    print(route_distance)
+    shortest_path = [
+        cities_locations_gdf["Label"][city_index] for city_index in shortest_path
+    ]
+    shortest_path = " -> ".join(shortest_path)
     return shortest_path, route
 
 
@@ -45,6 +76,9 @@ def main(api_key: str) -> None:
     shortest_path, route = get_optimal_path(
         cities_locations_gdf, distances, "Nairobi", "Kampala"
     )
+    print(shortest_path)
+    print(route)
+    utils.plot_cities(cities_locations_gdf, route)
 
 
 if __name__ == "__main__":
