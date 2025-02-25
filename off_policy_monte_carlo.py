54a26603cee6210d6f7e216f48c347b262fc7ee2
mirqwa <kimrodrikwa@gmail.com>
2025-02-25 12:31:25 +0300

    initialize behavior and target policies

--- off_policy_monte_carlo.py
+++ off_policy_monte_carlo.py
@@ -13,18 +13,23 @@ NO_OF_NEIGHBORS = 10
 def get_optimal_path(
     cities_locations_gdf: gpd.GeoDataFrame, distances: np.ndarray
 ) -> tuple:
-    policy = utils.initialize_policy(distances, NO_OF_NEIGHBORS)
+    behavior_policy = utils.initialize_policy(distances, NO_OF_NEIGHBORS)
+    target_policy = utils.initialize_policy(distances, NO_OF_NEIGHBORS)
     state_action_values = utils.initialize_state_action_values(
-        cities_locations_gdf, policy
+        cities_locations_gdf, behavior_policy
     )
 
     for episode in range(EPISODES):
         pass
 
+    shortest_path = route = None
+    return shortest_path, route
+
 
 def main(api_key: str) -> None:
     cities_locations_gdf, distances = utils.get_training_data(api_key)
-    utils.plot_cities(cities_locations_gdf)
+    # utils.plot_cities(cities_locations_gdf)
+    shortest_path, route = get_optimal_path(cities_locations_gdf, distances)
 
 
 if __name__ == "__main__":
